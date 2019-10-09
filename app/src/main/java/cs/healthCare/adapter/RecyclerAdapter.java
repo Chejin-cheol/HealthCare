85b2e0e8b3a1231f2feaefcc676b880aba544cfc
healergo <rhfo4030@gmail.com>
2019-10-09 11:37:37 +0900

    커밋 충돌

--- app/src/main/java/cs/healthCare/adapter/RecyclerAdapter.java
+++ app/src/main/java/cs/healthCare/adapter/RecyclerAdapter.java
@@ -2,6 +2,7 @@ package cs.healthCare.adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,6 +34,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemVi
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder holder, final int position){
         holder.onBind(listData.get(position));
+
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
